79a3b8365b16c2203b8330a20614e96976bedad0
Tuto <51621022+Tutojs@users.noreply.github.com>
2019-06-11 19:05:07 +0430

    Update token.js

--- token.js
+++ token.js
@@ -4,30 +4,42 @@
 let example_json = { name: 'Tuto', language: 'javascript', color: 'red'}
 
 // example template or API
-let template = 'welcome ${name}, your programming language is ${language}, favorite color is ${color}'
+let template = 'welcome ${name}, your programming language is ${language}, ${color} is your favorite color.'
 // '${' and '}' are default because ECMAscript template literal use them
-function token(template = '', obj = {}, signs = ['${', '}']) {
+function token(template = '', information = {}, signs = ['${', '}']) {
+  // custom signal signs START
   let signal = signs.map(element => element.replace(/[.*+?^${}()<>|[\]\\]/g, '\\$&'))
-
+  // custom signal signs END
+  
+  // raw tokens START
   let raw_tokens = template.match(RegExp(`${signal[0]}.*?${signal[1]}`, 'gs'))
+  // if no token find, return template without process
   if (raw_tokens == null) return template
   let { length } = raw_tokens
-  let raw = {}
+  // raw tokens END
+  
+  // tokens START
   let token = template.match(RegExp(`(?<=${signal[0]} *?)[^ ].*?(?= *?${signal[1]})`, 'gs'))
+  // token END
+  
+  let raw = {}
   let data = {}
+  
   for (let i = 0 ; i < length; i++) {
     raw[token[i]] = raw_tokens[i]
     data[token[i]] = token[i]
   }
-  data = {...data, ...obj}
+  // updating template token with information
+  data = {...data, ...information}
   for (let i = 0; i < length; i++) {
     template = template.replace( raw[token[i]], data[token[i]])
   }
   return template
 }
 
+// token(template: string, information
 // use: token(template, example_json)
-// will return: 'welcome Tuto, your programming language is javascript, favorite color is red'
+// will return: 'welcome Tuto, your programming language is javascript, red is your favorite color.'
 // if information aren't complate default values will return (default values are token names)
 // space between signal and token are removed 'this is an example ${  token with space  } in start end and middle'
 // raw token is: '${  token with space  }', this token use to replace with info
