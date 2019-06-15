d44bc597e3c9b1ef1dcac02582b7d8a2d1cbe63c
Tuto <51621022+Tutojs@users.noreply.github.com>
2019-06-15 15:59:59 +0430

    Update querystring.js

--- querystring.js
+++ querystring.js
@@ -1,18 +1,25 @@
 'use strict'
 // package.json: {"type": "module"}
-// node --experimental-modules querystring.js
+// node --experimental-modules query.js
 
 // modules START
+import url from 'url'
 import querystring from 'querystring'
 // modules END
 
-let querystring_api = function (api, information) {
+let node_querystring = function (api = '', information = {}) {
+  if (typeof api !== 'string') return `api must be string url, type of api is ${typeof api}.`
+  if (typeof information !== 'object') return `information must be object, type of information is ${typeof information}.`
 
-  let url = new URL(api)
+  let data = url.parse(api, true)
   // update search parameters of URL
-  let data = {...querystring.parse(url.query), ...information}
-  url.search = querystring.stringify(data)
-  return api
+  let { query } = data
+  let keys = Object.keys(query)
+  for (let key of keys) {
+    query[key] = information[key]
+  }
+  data.search = querystring.stringify(query)
+  return url.format(data)
 }
 
 /* https://nodejs.org : structure of URL
