37ddf6b5ecbbd4fcccb13d3cf81355c950fd93b7
Tuto <51621022+Tutojs@users.noreply.github.com>
2019-06-17 14:28:49 +0430

    Update token.js

--- token.js
+++ token.js
@@ -4,10 +4,10 @@ let token = function (template = '', information = {}, signs = ['${', '}']) {
   // error START
   let error =
     typeof template !== 'string' ?
-    `first argument (template) must be string, type of template is ${typeof template}.`
+    `first argument (template) must be string, type of template is ${Array.isArray(template) ? 'array' : typeof template}.`
     :
     typeof information !== 'object' && !Array.isArray(information) ?
-    `second argument (information) must be object, type of information is ${typeof information}.`
+    `second argument (information) must be object, type of information is ${Array.isArray(information) ? 'array' : typeof information}.`
     :
     !Array.isArray(signs) ?
     `third argument (signs) must be array, type of signs is ${typeof signs}.`
@@ -26,18 +26,51 @@ let token = function (template = '', information = {}, signs = ['${', '}']) {
     template.match(RegExp(`${signal[0]}.*?${signal[1]}`, 'gs')),
     template.match(RegExp(`(?<=${signal[0]} *?)[^ ].*?(?= *?${signal[1]})`, 'gs'))
   ]
-  if (tag[0] == null) return template
+  if (tag[0] === null) return template
   let { length } = tag[0]
   // tag END
 
+  // regular for loop START
   for (let i = 0 ; i < length; i++) {
+    let key = tag[1][i]
     template = template.replace(
       tag[0][i],
-      information[tag[1][i]] === undefined ?
-      tag[1][i] // default value
+      information[key] === undefined ?
+      key // default value
       :
-      information[tag[1][i]])
+      information[key]
+    )
   }
+  // regular for loop END
+  
+  // forEach loop START
+  /*
+  tag[1].forEach((key, i) => {
+    template = template.replace(
+      tag[0][i],
+      information[key] === undefined ?
+      key // default value
+      :
+      information[key]
+    )
+  })
+  */
+  // forEach loop END
+
+  // for ... of loop START
+  /*
+  for (let key of token[1]) {
+    template = template.replace(
+      tag[0][tag[1].indexOf(key)],
+      information[key] === undefined ?
+      key // default value
+      :
+      information[key]
+    )
+  }
+  */
+  // for ... of loop END
+
   
   return template
 }
